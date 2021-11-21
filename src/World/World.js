940cbcd736c15c4d9a4c99671d9b61148f99983d
Karthik Sundar <66824631+KarthikSundar2002@users.noreply.github.com>
2021-11-21 23:08:50 +0530

    Initial Commit

---
+++ src/World/World.js
@@ -0,0 +1,52 @@
+import { createCamera } from './components/camera.js';
+import { createLights } from './components/lights.js';
+import { createScene } from './components/scene.js';
+import { loadModels } from './components/models/model.js';
+
+import { createControls } from './systems/controls.js';
+import { createRenderer } from './systems/renderer.js';
+import { Resizer } from './systems/Resizer.js';
+import { Loop } from './systems/Loop.js';
+
+let camera;
+let renderer;
+let scene;
+let loop;
+
+class World {
+  constructor(container) {
+    camera = createCamera();
+    renderer = createRenderer();
+    scene = createScene();
+    loop = new Loop(camera, scene, renderer);
+    container.append(renderer.domElement);
+
+    const controls = createControls(camera, renderer.domElement);
+    const { ambientLight, mainLight } = createLights();
+
+    loop.updatables.push(controls);
+    scene.add(ambientLight, mainLight);
+
+    const resizer = new Resizer(container, camera, renderer);
+  }
+
+  async init() {
+    const {model} = await loadModels();
+    scene.add(model);
+    model.position.set(0,-1,0)
+  }
+
+  render() {
+    renderer.render(scene, camera);
+  }
+
+  start() {
+    loop.start();
+  }
+
+  stop() {
+    loop.stop();
+  }
+}
+
+export { World };
---
+++ src/World/components/models/model.js
@@ -0,0 +1,15 @@
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js';
+import { setupModel } from './setupModel.js';
+async function loadModels() {
+    const loader = new GLTFLoader();
+
+    const ModelData = await loader.loadAsync('/assets/models/leather.gltf');
+
+    console.log("Model", ModelData);
+
+    const model = setupModel(ModelData);
+
+    return { model };
+}
+
+export { loadModels };
